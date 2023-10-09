2a008bd5a1c472d2b223d20559413460fe76d463
Jorgesolan <816259@unizar.es>
2023-10-09 13:53:51 +0200

    Representacion de la luz

--- src/simulacion.hs
+++ src/simulacion.hs
@@ -55,13 +55,13 @@ generateRaysForPixels (Camara p (Base (Direction px _ _) (Direction _ py _) (Dir
 --         angle = (normal .* ligthDir) / ((modd normal) * (modd ligthDir))
 --         newRgb = rgbProd rgb (1 - (angle / 90.0))
 
-luzXRayo :: [(Float, (RGB, Point3D, Direction))] -> [(Float, (RGB, Point3D, Direction))] -> [RGB]
+luzXRayo :: [(Float, (RGB, Point3D, Direction))] -> [(Float, (RGB, Point3D, Direction))] -> [(Float, (RGB, Point3D, Direction))]
 luzXRayo = zipWith eligeResultado
   where
-  eligeResultado :: (Float, (RGB, Point3D, Direction)) -> (Float, (RGB, Point3D, Direction)) -> RGB
-  eligeResultado (_, (rgb1@(RGB r g b), pa, _)) (_,(_, pb, _))
-    | (aproxPoint pa pb) = rgb1
-    | otherwise = (RGB (r/3) (g/3) (b/3))
+  eligeResultado :: (Float, (RGB, Point3D, Direction)) -> (Float, (RGB, Point3D, Direction)) -> (Float, (RGB, Point3D, Direction))
+  eligeResultado a@(t, (rgb1@(RGB r g b), pa, d)) (_,(_, pb, _))
+    | (aproxPoint pa pb) = a
+    | otherwise = (t,(RGB (r/3) (g/3) (b/3),pa,d))
 
 
 listRay ::  [[(Float, (RGB, Point3D, Direction))]] -> [(Float, (RGB, Point3D, Direction))]
@@ -85,7 +85,7 @@ basCam = Base (Direction piCam 0 0) (Direction 0 piCam 0) (Direction 0 0 (-500))
 centr = Point3D (0) (0) 0
 centr' = Point3D (-50) 200 0
 triangulo = Triangulo (Point3D (5) (25) 70) (Point3D (15) (5) 70) (Point3D (5) (5) 70) (RGB 255 0 255)
-luz = Point3D (0) (-175) (0)
+luz = Point3D (100) (-175) (-20)
 
 plano0 = Plane (Plano (Point3D (-200) 0 200) (Direction 1 0 0) (RGB 249 176 84))
 plano1 =  Plane (Plano (Point3D (200) 0 200) (Direction (1) (0) (0)) (RGB 146 223 222))
@@ -94,6 +94,7 @@ plano3 =  Plane (Plano (Point3D 0 0 200) (Direction 0 0 (-1)) (RGB 175 170 169))
 plano4 =  Plane (Plano (Point3D 0 (-250) 200) (Direction 0 (-1) (0)) (RGB 255 0 255))
 bola =  Sphere (Esfera centr 50 (RGB 255 0 0))
 bola' =  Sphere (Esfera centr' 40 (RGB 0 0 255))
+bolaLus = Sphere (Esfera luz 10 (RGB 255 255 255))
 camara = Camara (Point3D (0) (0) (-1000)) basCam
 
 -- Función para extraer los puntos de las tuplas
@@ -106,13 +107,13 @@ main = do
       start <- getCPUTime
       let rayitos = generateRaysForPixels camara pix pix `using` parListChunk 32 rseq
       let sol = force parametricShapeCollision [bola,bola',plano0,plano1,plano2,plano3,plano4] rayitos `using` parListChunk 32 rseq
+      let solBolaLus =  force parametricShapeCollision [bolaLus] rayitos
       let solo = listRay sol
       let lusesita = force map (\punto -> Ray luz (punto #< luz) 0) $ obtenerPuntos solo 
       let solLus = force parametricShapeCollision [bola,bola',plano0,plano1,plano2,plano3,plano4] lusesita `using` parListChunk 32 rseq
       let sol' = luzXRayo (solo) (listRay solLus)
-      let a = map rgbToString sol'
-       --print a
-      --let a = concat $ map rgbToString $ listRayToRGB luz sol'
+      let sol'' = listRay [sol',(concat solBolaLus)]
+      let a = map rgbToString $ map (\(_,(rgb,_,_)) -> rgb) sol''
       writePPM "a.ppm" (round pix) (round pix) (concat a)
       end <- getCPUTime
       let diff = fromIntegral (end - start) / (10^12) :: Double
