a9d5c6854868fd11ba27b882bd32242bbc40779a
FranciscoJavierPizarro <821259@unizar.es>
2023-12-19 09:30:09 +0100

    compilación para labs

--- tmp/distributed_compilation.sh
+++ tmp/distributed_compilation.sh
@@ -4,16 +4,23 @@ zip -r original_graf.zip ../../grafica/
 mv original_graf.zip ..
 scp ../original_graf.zip 192.168.0.252:~/
 ssh -n 192.168.0.252 'unzip original_graf.zip;cd grafica;docker-compose up'
-scp 192.168.0.252:~/grafica/tmp/simulacion_pilgor .
-scp 192.168.0.252:~/grafica/tmp/cargaKD_pilgor .
+scp 192.168.0.252:~/grafica/tmp/simulacion .
+scp 192.168.0.252:~/grafica/tmp/cargaKD .
 ssh -n 192.168.0.252 'sudo rm -rf ./grafica original_graf.zip'
 #crear ejec berl
+mv simulacion simulacion_pilgor
+mv cargaKD cargaKD_pilgor
 cd ..
 make all
 cd tmp
 mv simulacion simulacion_berlin
 mv cargaKD cargaKD_berlin
-
+#crear ejec labs
+cd ..
+docker-compose up
+cd tmp
+mv simulacion simulacion_lab102
+mv cargaKD cargaKD_lab102
 #crear comprimido con todo para llevarlo a los entornos de ejec
 zip -r compilados.zip ../../grafica/meshes ../../grafica/tmp
 #copiar comprimido a central
--- tmp/distributed_run.rb
+++ tmp/distributed_run.rb
@@ -14,28 +14,28 @@ end.parse!
 ################################################################################
 
 NODES = [
-  { hostname: 'pilgor', ip: 'pilgor.cps.unizar.es', cpus: 90 },
-  { hostname: 'berlin', ip: 'berlin.unizar.es', cpus: 30 },
-  { hostname: 'lab102-191', ip: '155.210.154.191', cpus: 6 },
-  { hostname: 'lab102-192', ip: '155.210.154.192', cpus: 6 },
-  { hostname: 'lab102-193', ip: '155.210.154.193', cpus: 6 },
-  { hostname: 'lab102-194', ip: '155.210.154.194', cpus: 6 },
-  { hostname: 'lab102-195', ip: '155.210.154.195', cpus: 6 },
-  { hostname: 'lab102-196', ip: '155.210.154.196', cpus: 6 },
-  { hostname: 'lab102-197', ip: '155.210.154.197', cpus: 6 },
-  { hostname: 'lab102-198', ip: '155.210.154.198', cpus: 6 },
-  { hostname: 'lab102-199', ip: '155.210.154.199', cpus: 6 },
-  { hostname: 'lab102-200', ip: '155.210.154.200', cpus: 6 },
-  { hostname: 'lab102-201', ip: '155.210.154.201', cpus: 6 },
-  { hostname: 'lab102-202', ip: '155.210.154.202', cpus: 6 },
-  { hostname: 'lab102-203', ip: '155.210.154.203', cpus: 6 },
-  { hostname: 'lab102-204', ip: '155.210.154.204', cpus: 6 },
-  { hostname: 'lab102-205', ip: '155.210.154.205', cpus: 6 },
-  { hostname: 'lab102-206', ip: '155.210.154.206', cpus: 6 },
-  { hostname: 'lab102-207', ip: '155.210.154.207', cpus: 6 },
-  { hostname: 'lab102-208', ip: '155.210.154.208', cpus: 6 },
-  { hostname: 'lab102-209', ip: '155.210.154.209', cpus: 6 },
-  { hostname: 'lab102-210', ip: '155.210.154.210', cpus: 6 },
+  { hostname: 'pilgor', ip: 'pilgor.cps.unizar.es', cpus: 90, binExt: "pilgor" },
+  { hostname: 'berlin', ip: 'berlin.unizar.es', cpus: 30, binExt: "berlin" },
+  { hostname: 'lab102-191', ip: '155.210.154.191', cpus: 6, binExt: "lab102" },
+  { hostname: 'lab102-192', ip: '155.210.154.192', cpus: 6, binExt: "lab102" },
+  { hostname: 'lab102-193', ip: '155.210.154.193', cpus: 6, binExt: "lab102" },
+  { hostname: 'lab102-194', ip: '155.210.154.194', cpus: 6, binExt: "lab102" },
+  { hostname: 'lab102-195', ip: '155.210.154.195', cpus: 6, binExt: "lab102" },
+  { hostname: 'lab102-196', ip: '155.210.154.196', cpus: 6, binExt: "lab102" },
+  { hostname: 'lab102-197', ip: '155.210.154.197', cpus: 6, binExt: "lab102" },
+  { hostname: 'lab102-198', ip: '155.210.154.198', cpus: 6, binExt: "lab102" },
+  { hostname: 'lab102-199', ip: '155.210.154.199', cpus: 6, binExt: "lab102" },
+  { hostname: 'lab102-200', ip: '155.210.154.200', cpus: 6, binExt: "lab102" },
+  { hostname: 'lab102-201', ip: '155.210.154.201', cpus: 6, binExt: "lab102" },
+  { hostname: 'lab102-202', ip: '155.210.154.202', cpus: 6, binExt: "lab102" },
+  { hostname: 'lab102-203', ip: '155.210.154.203', cpus: 6, binExt: "lab102" },
+  { hostname: 'lab102-204', ip: '155.210.154.204', cpus: 6, binExt: "lab102" },
+  { hostname: 'lab102-205', ip: '155.210.154.205', cpus: 6, binExt: "lab102" },
+  { hostname: 'lab102-206', ip: '155.210.154.206', cpus: 6, binExt: "lab102" },
+  { hostname: 'lab102-207', ip: '155.210.154.207', cpus: 6, binExt: "lab102" },
+  { hostname: 'lab102-208', ip: '155.210.154.208', cpus: 6, binExt: "lab102" },
+  { hostname: 'lab102-209', ip: '155.210.154.209', cpus: 6, binExt: "lab102" },
+  { hostname: 'lab102-210', ip: '155.210.154.210', cpus: 6, binExt: "lab102" },
 ]
 
 ################################################################################
@@ -67,7 +67,7 @@ if !shutdown
       ping_successful = system("ping -c 1 #{node[:hostname]}")
       if ping_successful
         system(ssh_command + "'" +"cp grafica /tmp" + "'")
-        spawn(ssh_command + "'" + "cd /tmp/grafica/tmp; ./run_param.sh #{currentcpus} #{currentcpus + node[:cpus]} #{TOTAL_CPUS} #{node[:hostname]} ; cd /tmp; rm -rf ./grafica" + "'")
+        spawn(ssh_command + "'" + "cd /tmp/grafica/tmp; ./run_param.sh #{currentcpus} #{currentcpus + node[:cpus]} #{TOTAL_CPUS} #{node[:binExt]} ; cd /tmp; rm -rf ./grafica" + "'")
         currentcpus += node[:cpus]
       else
         puts "#{node} is fallen"
