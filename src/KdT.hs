7fd708a929183ed3a842c706faf0a28134a20506
FranciscoJavierPizarro <821259@unizar.es>
2024-01-11 16:49:34 +0100

    limpieza y comentado de código de KdT y Tone_map

--- src/KdT.hs
+++ src/KdT.hs
@@ -5,9 +5,11 @@ import Elem3D ( Foton(..), Point3D(..) )
 
 
 {-# INLINE fotonAxis #-}
+-- |Función auxiliar, dado un fotón devuelve una lista con sus 3 coordenadas.
 fotonAxis :: Foton -> [Float]
 fotonAxis (Foton {pFot = Point3D x y z}) = [x, y, z] -- Indicamos simplemente de donde coge las coordenadas paral el kdTree 
 
 {-# INLINE createKD #-}
+-- |Función básica, dado una lista de fotones, se crea la estructura KDT correspondiente.
 createKD :: [Foton] -> KdTree Float Foton
 createKD = Data.KdTree.Static.build fotonAxis -- Creación del kdTree
--- src/Tone_map.hs
+++ src/Tone_map.hs
@@ -3,26 +3,32 @@ module Tone_map where
 import Elem3D ( RGB(..), elevateRGBPoint )
 
 {-# INLINE clamp #-}
+-- |Función auxiliar, dado un valor máximo y una lista de RGBs, aplica la operación de clampeo con dicho valor sobre estos.
 clamp :: Float -> [RGB] -> [RGB]
 clamp x = map (\(RGB {..}) -> RGB (min x red) (min x green) (min x blue))
 
 {-# INLINE ecualization #-}
+-- |Función auxiliar, dado un valor y una lista de RGBs, aplica la operación de ecualización con dicho valor sobre estos.
 ecualization :: Float -> [RGB] -> [RGB]
 ecualization x = map (\(RGB {..}) -> RGB (red / x) (green / x) (blue / x))
 
 {-# INLINE ecualization' #-}
+-- |Función auxiliar, dado un valor y una lista de RGBs, revierte la operación de ecualización con dicho valor sobre estos.
 ecualization' :: Float -> [RGB] -> [RGB]
 ecualization' x = map (\(RGB {..}) -> RGB (red * x) (green * x) (blue * x))
 
 {-# INLINE elevateRGBPoints #-}
+-- |Función auxiliar, dado un valor y una lista de RGBs, eleva estos a 1/valor.
 elevateRGBPoints :: Float -> [RGB] -> [RGB]
 elevateRGBPoints x = map (elevateRGBPoint x)
 
 {-# INLINE gammaFunc #-}
+-- |Función auxiliar, dado dos valores y una lista de RGBs, aplica la función gamma con dichos valores sobre estos.
 gammaFunc :: Float -> Float -> [RGB] -> [RGB]
 gammaFunc x gamma = ecualization' 1 . elevateRGBPoints gamma . ecualization x
 
 {-# INLINE gammaFunc' #-}
+-- |Función auxiliar, dado dos valores y una lista de RGBs, revierte la aplicación de la función gamma con dichos valores sobre estos.
 gammaFunc' :: Float -> Float -> [RGB] -> [RGB]
 gammaFunc' x gamma = ecualization' 1 . elevateRGBPoints (1 / gamma) . ecualization x
 
